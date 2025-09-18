afa06682b9da1308853c469bfa130299e31b257d
Michael <michaelkindye@gmail.com>
2025-09-18 14:57:12 +0300

    fixed case sensitive imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-import Home from '@/components/home/home';
-import Login from '@/components/login/login';
-import SignUp from '@/components/signup/signup';
+import Home from '@/components/home/Home.jsx';
+import Login from '@/components/login/login.jsx';
+import SignUp from '@/components/signup/signup.jsx';
 
 const App = () => {
   return (
